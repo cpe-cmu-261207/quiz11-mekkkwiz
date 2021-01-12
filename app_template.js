18cf8d3ac08f2f0c8c15d4a469e83f6cb2890aa8
mekkkwiz <59276758+mekkkwiz@users.noreply.github.com>
2021-01-12 13:56:57 +0700

    1 done

--- app_template.js
+++ app_template.js
@@ -1,1 +1,18 @@
 //your code here!
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('#button-compute').onclick = function(){
+        const input = document.querySelector('#input-number').value
+        console.log(input)
+
+        document.querySelector('#ans1').innerHTML = 620610807-input
+    }
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('#button-create-table').onclick = function(){
+        const row = document.querySelector('#input-row').value
+        const col = document.querySelector('#input-col').value
+
+        
+    }
+})
